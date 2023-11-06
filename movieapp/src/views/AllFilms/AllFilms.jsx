0baa9843fc21a2974625caa2726385b9637f5d92
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-11-06 18:55:17 +0300

    MovieApp-Search
    All Films sayfasında search işlemi gerçekleştirildi.

--- movieapp/src/views/AllFilms/AllFilms.jsx
+++ movieapp/src/views/AllFilms/AllFilms.jsx
@@ -1,30 +1,41 @@
-import FilterItems from '../../common/FilterItems/FilterItems';
-import MoviesContainer from '../../common/MoviesContainer/MoviesContainer';
-import {useDispatch, useSelector} from 'react-redux'
-import { useState, useEffect } from 'react';
-import {getAllMovies} from '../../utils/requests'
+import FilterItems from "../../common/FilterItems/FilterItems";
+import MoviesContainer from "../../common/MoviesContainer/MoviesContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { getAllMovies } from "../../utils/requests";
 
 const AllFilms = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const movies = useSelector((state) => state.movies.movies);
 
-  const [search, setSearch] = useState()
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  const [search, setSearch] = useState();
 
   useEffect(() => {
-    dispatch(getAllMovies())
-  }, [])
+    dispatch(getAllMovies());
+  }, []);
 
   useEffect(() => {
-    console.log(search);
-  }, [search])
+    let tempMovies = [...movies];
 
-  
+    if (search) {
+      tempMovies = tempMovies.filter((movie) =>
+        movie.title.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    setFilteredMovies(tempMovies);
+  }, [search, movies.length]);
+
+  console.log(filteredMovies);
+
   return (
     <>
-      <FilterItems setSearch= {setSearch}/>
-      <MoviesContainer title='All Films' movies={movies} />
+      <FilterItems setSearch={setSearch} />
+      <MoviesContainer title="All Films" movies={filteredMovies} />
     </>
-  )
-}
+  );
+};
 
-export default AllFilms
+export default AllFilms;
