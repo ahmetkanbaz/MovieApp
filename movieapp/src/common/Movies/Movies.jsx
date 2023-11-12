7795e8a8bebcc8012511f9944cce6a79749da157
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-11-12 19:44:35 +0300

    MovieApp-Card-Icons-Add

--- movieapp/src/common/Movies/Movies.jsx
+++ movieapp/src/common/Movies/Movies.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
 import Card from "../Card/Card";
 import DangerAlert from "../DangerAlert/DangerAlert";
+import { useLocation } from "react-router-dom";
+
 const Movies = ({ movies }) => {
+  const location = useLocation()
+
+  const isAllMovies = location.pathname == '/allmovies'
+
   return (
     <div className="row justify-content-center g-3">
       {
         movies.length > 0 ? (
           movies.map((movie) => (
-            <Card key={movie.id} movie={movie} />
+            <Card key={movie.id} movie={movie} isAllMovies = {isAllMovies} />
           ))
         ) : (
           <DangerAlert title='Movie Not Found!'/>
