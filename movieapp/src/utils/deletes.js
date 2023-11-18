e97c7b687ecd341d72a4165bda7af5961f7c2e49
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-11-18 11:37:34 +0300

    MovieApp-DeleteMovie-Completed
    Filmlerin silinebilmesi sağlandı. Json-server içerisinden ve ekrandan da silinebilmesi sağlandı. Aynı zamanda güncel verilerin de ekranda görüntülenebilmesi sağlandı.

--- movieapp/src/utils/deletes.js
+++ movieapp/src/utils/deletes.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
-const baseUrl = import.meta.env.
+const baseUrl = import.meta.env.VITE_BASE_URL
 
 export const deleteMovie = async (id) => {
-  try {
-    const response = await axios.delete()
-  }
+    const response = await axios.delete(`${baseUrl}/movies/${id}`)
+    if (response.status == 200) {
+      return `The movie with ID ${id} was deleted successfully.`
+    }
+    else {
+      return `An error was encountered while deleting the movie.`
+    }
 }
