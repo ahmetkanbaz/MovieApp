c2d35f85458bd9d6657d9719dd638fc6651638e0
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-11-23 21:55:53 +0300

    MovieApp-UpdateModal-Style

--- movieapp/src/common/UpdateModal/UpdateModal.jsx
+++ movieapp/src/common/UpdateModal/UpdateModal.jsx
@@ -1,36 +1,78 @@
-import PropTypes from 'prop-types'
-import {useSelector} from 'react-redux'
-import { UpdateModalWrapper } from './UpdateModalStyle'
-const UpdateModal = ({movie}) => {
-  const theme = useSelector((state) => state.theme.theme)
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { UpdateModalWrapper } from "./UpdateModalStyle";
+const UpdateModal = ({ movie }) => {
+  const theme = useSelector((state) => state.theme.theme);
 
-  console.log(movie)
+  const {
+    title,
+    year,
+    released,
+    runtime,
+    genre,
+    director,
+    writer,
+    actors,
+    plot,
+    language,
+    country,
+    awards,
+    poster,
+    imdbRating,
+    production,
+  } = movie || {};
 
   return (
     <UpdateModalWrapper theme={theme}>
-<div className="modal fade" id="updateMovieModal" tabIndex="-1" aria-labelledby="updateMovieModal" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="updateMovieModal">Update Movie</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div
+        className="modal fade"
+        id="updateMovieModal"
+        tabIndex="-1"
+        aria-labelledby="updateMovieModal"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="updateMovieModal">
+                Update Movie
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">Title</label>
+                  <input type="text" id="title" name="title" className="form-control" defaultValue={title}/>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="modal-body">
-        Update Modal
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
     </UpdateModalWrapper>
-  )
-}
+  );
+};
 
 UpdateModal.propTypes = {
-  movie: PropTypes.object
-}
+  movie: PropTypes.object,
+};
 
-export default UpdateModal
+export default UpdateModal;
--- movieapp/src/common/UpdateModal/UpdateModalStyle.js
+++ movieapp/src/common/UpdateModal/UpdateModalStyle.js
@@ -4,5 +4,13 @@ export const UpdateModalWrapper = styled.div`
   .modal-content {
     background-color: ${({ theme }) =>
       theme == "light" ? "#F5F5F5" : "#333333"};
+    input {
+      color: ${({ theme }) => (theme == "light" ? "#000000" : "#F5F5F5")};
+      &:focus {
+        border-color: #000000;
+        background-color: ${({ theme }) =>
+          theme == "light" ? "#F5F5F5" : "#636363"};
+      }
+    }
   }
 `;
