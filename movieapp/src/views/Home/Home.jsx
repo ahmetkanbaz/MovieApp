ade519dae6c6a88cbe73517f9a12fe910aa03d21
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2024-01-08 18:48:34 +0300

    MovieApp-Update

--- movieapp/src/views/Home/Home.jsx
+++ movieapp/src/views/Home/Home.jsx
@@ -1,20 +1,24 @@
 import Banner from "../../common/Banner/Banner";
 import MoviesContainer from "../../common/MoviesContainer/MoviesContainer";
-import {useDispatch, useSelector} from 'react-redux'
-import {useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { getAllMovies } from "../../utils/requests";
 import PopularActors from "../../components/PopularActors/PopularActors";
 
 const Home = () => {
-  const dispatch = useDispatch()
-  const movies = useSelector((state) => state.movies.movies)
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movies.movies);
 
-  const latestMovies = [...movies].sort((a, b) => new Date(b.released) - new Date(a.released)).slice(0, 6)
-  const hightRating = [...movies].sort((a, b) => b.imdbRating - a.imdbRating).slice(0, 6)
+  const latestMovies = [...movies]
+    .sort((a, b) => new Date(b.released) - new Date(a.released))
+    .slice(0, 6);
+  const hightRating = [...movies]
+    .sort((a, b) => b.imdbRating - a.imdbRating)
+    .slice(0, 6);
 
   useEffect(() => {
-    dispatch(getAllMovies())
-  }, [])
+    dispatch(getAllMovies());
+  }, []);
 
   return (
     <>
@@ -23,9 +27,17 @@ const Home = () => {
         content="Every Movie, A New Adventure"
         imageUrl="https://plus.unsplash.com/premium_photo-1682125771198-f7cbed7cb868?auto=format&fit=crop&q=80&w=2060&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
-      <MoviesContainer className = 'thisYear' title='Latest Movies' movies={latestMovies} />
+      <MoviesContainer
+        className="homeMovies"
+        title="Latest Movies"
+        movies={latestMovies}
+      />
       <PopularActors />
-      <MoviesContainer title='Movies with the Highest Ratings' movies={hightRating} />
+      <MoviesContainer
+        className="homeMovies"
+        title="Movies with the Highest Ratings"
+        movies={hightRating}
+      />
     </>
   );
 };
